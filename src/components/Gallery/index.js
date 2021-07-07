b784a0863c73efc6314514cf7585a9d6a3a1e2ed
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-07 18:19:13 +0400

    Gallery component created

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+function Gallery() {
+    return (
+        <section>
+            <h1>Name</h1>
+            <p>Description</p>
+        </section>
+    );
+}
+
+module.exports = Gallery;
